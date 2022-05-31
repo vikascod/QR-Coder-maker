47e894064bd4f832965225dd99fff7dbd388e9db
Vikas <vikas1618072@gmail.com>
2022-05-31 15:18:27 +0800

    QR code maker

---
+++ main.py
@@ -0,0 +1,18 @@
+import qrcode
+# from PIL import Image
+
+# qr_maker = qrcode.QRCode(version=1,
+#                          error_correction=qrcode.constants.ERROR_CORRECT_H,
+#                          box_size=15, border=4)
+
+# qr_maker.add_data("https://www.youtube.com/watch?v=2Gja5wRgsCg")
+# qr_maker.make(fit=True)
+
+# Img = qr_maker.make_image(fill_color='red', back_color='white')
+# Img.save("New_QR_code_Scanner.png")
+
+
+
+
+# vik = qrcode.make("https://www.youtube.com/watch?v=96hyQ6bt-b8")
+# vik.save("ncs_song.png")
